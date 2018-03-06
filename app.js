553b890b0b1eb0bd2f07049a092c0c4af2e58b2a
robertruhiu <robertruhiu@gmail.com>
2018-03-06 17:08:28 +0000

    full  rebase to enable rework

--- app.js
+++ app.js
@@ -1,59 +1,61 @@
 var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-var expressHbs = require('express-handlebars');
-
-var routes = require('./routes/index');
-
 var app = express();
 
-// view engine setup
-app.engine('.hbs', expressHbs({defaultLayout: 'layout', extname: '.hbs'}));
-app.set('view engine', '.hbs');
+var cookieParser = require('cookie-parser');
+var session = require('express-session');
+var morgan = require('morgan');
+var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+var passport = require('passport');
+var flash = require('connect-flash');
+var mongo = require('mongodb');
+var assert = require('assert');
+var expressHbs = require('express-handlebars');
+var path = require('path');
+var MongoStore = require('connect-mongo')(session);
+
+require('./config/passport')(passport);
+
+
+
+app.use(morgan('dev'));
+app.use(cookieParser());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
+
+
+
+var configDB = require('./config/database.js');
+mongoose.connect(configDB.url);
+app.use(session({secret: '#tag#icui4cu#',
+	saveUninitialized: true,
+	resave: false,
+	store:new MongoStore({mongooseConnection:mongoose.connection}),
+	cookie:{maxAge:100*60*1000}
+}));
+
+app.use(passport.initialize());
+app.use(passport.session()); // persistent login sessions
+app.use(flash()); // use connect-flash for flash messages stored in session
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+
+//Create public directory for views
+app.engine('.hbs', expressHbs());
+app.set('view engine', '.hbs');
+app.use(function (req,res,next) {
+	res.locals.login = req.isAuthenticated();
+	res.locals.session = req.session;
+	next();
+
 });
 
-// error handlers
-
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
-    });
-  });
-}
-
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
-});
+require('./routes/routes.js')(app, passport);
 
 
 module.exports = app;
